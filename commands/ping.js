5a57d7cfc8747565b41c50e2e1619f304f3a88c0
Rohil Tuli <rohil.tuli@gmail.com>
2021-10-23 19:30:12 -0400

    trying out embed in discord

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,31 @@
 const {SlashCommandBuilder} = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
       .setName('ping')
       .setDescription('Wakes up the bot for a quickie'),
   async execute(interaction) {
-    await interaction.reply('I AM ALIVEEEEE');
-    await interaction.followUp('FEEEEEEEEEEEED MEEEEEEEEEEE');
+    const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org/')
+        .setAuthor('Some name', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+        .setDescription('Some description here')
+        .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+        .addFields(
+            {name: 'Inline title', value: 'Some value \nhere', inline: true},
+            {name: 'Inline title', value: 'Some value \nhere', inline: true},
+            {name: 'Inline title', value: 'Some value \nhere', inline: true},
+        )
+        .setImage('https://i.imgur.com/AfFp7pu.png')
+        .setTimestamp()
+        .setFooter('Some footer text here', 'https://i.imgur.com/AfFp7pu.png');
+
+    interaction.channel.send({embeds: [exampleEmbed]});
+    interaction.reply(
+        // eslint-disable-next-line max-len
+        'Okay, Here are the current transactions I\'ve kept track of for this trip',
+    );
   },
 };
