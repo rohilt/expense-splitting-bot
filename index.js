7dde9d267605539e34b7d92cef5bf028bd29997b
Rohil Tuli <rohil.tuli@gmail.com>
2021-10-23 13:08:49 -0400

    added in linter

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-require('dotenv').config()
+const {Client, Intents} = require('discord.js');
+require('dotenv').config();
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
 // Login to Discord with your client's token
